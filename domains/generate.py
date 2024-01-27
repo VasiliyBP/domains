2bb2af74720f3eae7dc1ca00ab463b4c3eadee3f
Vasiliy Bogoyavlesnkiy <VasiliyBP@users.noreply.github.com>
2024-01-27 04:19:58 +0300

    Delete domains directory

--- domains/generate.py
+++
@@ -1,8 +0,0 @@
-newf=""
-with open('domains.txt','r') as f:
-    for line in f:
-        newf+= "||" + line.strip() + "\n"
-    f.close()
-with open('domains-done.txt','w') as f:
-    f.write(newf)
-    f.close()
